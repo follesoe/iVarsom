c691616c0492397e5eda1e6b0fbac86907d123e6
Jonas Follesø <jonas@follesoe.no>
2023-12-24 15:41:59 +0100

    #9 Improve drawing of Expositions view
    Improve centring of the N-label drawn in the North sector.
    
    Close #9.

--- iVarsom/Views/Expositions.swift
+++ iVarsom/Views/Expositions.swift
@@ -5,16 +5,24 @@ struct Expositions: View {
     let sectors: [Bool]
         
     var body: some View {
+        let nSectors = sectors.count
+        let sectorDeg = 360.0 / Double(nSectors)
+        let halfSectorDeg = sectorDeg / 2.0
+        let lineWidth = 1.0
+        let doubleLineWidth = lineWidth * 2.0
         let strokeColor = Color("LightStroke")
         
         Canvas { context, size in
             context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
+            
             var pieContext = context
             pieContext.rotate(by: .degrees(-90))
-            let radius = min(size.width, size.height) * 0.42
-            var startAngle = Angle(degrees: -22.5)
+            
+            let radius = min(size.width, size.height) * 0.41
+            var startAngle = Angle(degrees: -halfSectorDeg)
+            
             for sector in sectors {
-                let angle = Angle(degrees: 45)
+                let angle = Angle(degrees: sectorDeg)
                 let endAngle = startAngle + angle
                 let path = Path { p in
                     p.move(to: .zero)
@@ -23,29 +31,39 @@ struct Expositions: View {
                 }
                 
                 pieContext.fill(path, with: .color(Color(sector ? "DangerFill" : "NoDangerFill")))
-                pieContext.stroke(path, with: .color(strokeColor), lineWidth: 1)
+                pieContext.stroke(path, with: .color(strokeColor), lineWidth: lineWidth)
                 startAngle = endAngle
             }
             
             pieContext.rotate(by: .degrees(90))
         
             let headingRadius = radius / 4
+            let elipseRect = CGRect(
+                x: -headingRadius,
+                y: -(size.height / 2) + 2,
+                width: headingRadius * 2,
+                height: headingRadius * 2)
+            
             let path = Path { p in
-                p.addEllipse(in: CGRect(
-                    x: -headingRadius,
-                    y: -(size.height / 2) + 2,
-                    width: headingRadius * 2,
-                    height: headingRadius * 2))
+                p.addEllipse(in: elipseRect)
             }
             pieContext.fill(path, with: .color(.white))
-            pieContext.stroke(path, with: .color(strokeColor), lineWidth: 1)
+            pieContext.stroke(path, with: .color(strokeColor), lineWidth: lineWidth)
+                        
+            let resolvedN = context.resolve(
+                Text("N")
+                    .font(.system(size: headingRadius * 1.5))
+                    .bold()
+                    .foregroundStyle(.black)
+            )
+            
+            let sizeN = resolvedN.measure(in: elipseRect.size)
             
             pieContext.draw(
-                Text("N")
-                    .font(.system(size: headingRadius * 1.4))
-                    .bold()
-                    .foregroundStyle(.black),
-                at: CGPoint(x: 0, y: -(size.height / 2) + headingRadius))
+                resolvedN,
+                at: CGPoint(
+                    x: elipseRect.midX,
+                    y: elipseRect.midY - (sizeN.height / elipseRect.height)), anchor: .center)
 
             pieContext.rotate(by: .degrees(90))
             
@@ -54,8 +72,14 @@ struct Expositions: View {
     }
 }
 
-#Preview {
+#Preview("Default view") {
     Expositions(sectors: [
         true, true, true, false, false, false, false, false
     ])
+}
+
+#Preview("Small view") {
+    Expositions(sectors: [
+        true, true, true, false, false, false, false, false
+    ]).frame(width: 64, height: 64)
 }
